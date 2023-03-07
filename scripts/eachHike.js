ed1e1fc68895ffce5ff5211c1d8de5486d302baf
oceaan-pendharkar <oceaanpendharkar@gmail.com>
2023-03-07 09:29:12 -0800

    hike review form implemented

--- scripts/eachHike.js
+++ scripts/eachHike.js
@@ -18,3 +18,10 @@ function displayHikeInformation() {
     )
 }
 displayHikeInformation();
+
+function saveHikeDocumentIDAndRedirect() {
+    let params = new URL(window.location.href) //get the url from the search bar
+    let ID = params.searchParams.get("docID");
+    localStorage.setItem('hikeDocID', ID);
+    window.location.href = 'review.html';
+}
